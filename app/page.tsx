28886d3a6d0ebab7148c29f02d9f28b162b083ae
Christian Sousa <christiansousadev@gmail.com>
2024-07-23 15:03:28 -0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,53 @@
-import Image from "next/image";
- 
-export function MenuDefault() {
-  return (
-    <Menu>
-      <MenuHandler>
-        <Button>Menu</Button>
-      </MenuHandler>
-      <MenuList>
-        <MenuItem>Menu Item 1</MenuItem>
-        <MenuItem>Menu Item 2</MenuItem>
-        <MenuItem>Menu Item 3</MenuItem>
-      </MenuList>
-    </Menu>
-  );
-}
+// pages/index.tsx
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header/Header';
 
 
-export default function Home() {
+/**
+ * The Home page component.
+ *
+ * This component renders the main landing page of the Morry crypto trading platform.
+ *
+ * @returns {JSX.Element} The JSX element representing the Home page.
+ * @example
+ * <Home />
+ */
+
+const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-5 ml-10 m-5">
-      <header className="flex justify-between">
-          <div className="flex container flex-row gap-10 ">
-              <div className="header-left flex gap-2">
-                  <Image
-                    src="/favicon.ico"
-                    width={25}
-                    height={25}
-                    alt="Picture of the author"
-                  />
-                  <a href='#' className='font-bold text-white'>Morry</a>
-              </div>
-              <nav>
-                <ul className="flex flex-row gap-5">
-                  <li><a href="#" className="text-white">Home</a></li>
-                  <li><a href="#" className="text-white">Service</a></li>
-                  <li><a href="#" className="text-white">Market</a></li>
-                  <li><a href="#" className="text-white">Community</a></li>
-                  <li><a href="#" className="text-white">Blog</a></li>
-                  <li><a href="#" className="text-white">About</a></li>
-                </ul>
-              </nav>
-          </div>
-          <div>
-            <menu className="">
-              
-            </menu>
-          </div>
-      </header>
-    </main>
+    <div className="bg-black text-white min-h-screen">
+      <Head>
+        <title>Morry - Crypto Trading Platform</title>
+      </Head>
+
+      <Header />
+
+      <main className="flex flex-col justify-center flex-1 px-4 pl-10 overflow-hidden">
+        <div className="mt-20">
+        <h1 className="text-7xl font-semibold mt-5">The safest</h1>
+        <h1 className="text-7xl font-semibold mt-5">cryptocurrency</h1>
+        <h1 className="text-7xl font-semibold mt-5">trading platform</h1>
+        </div>
+        <div className='mt-4'>
+        <p className="text-base text-gray-500">
+          Morry is a new platform that provides more than 10,000+
+        </p>
+        <p className="text-base text-gray-500"> markets to buy and sell </p>
+        </div>
+        <div className="mt-8 flex w-[25rem] h-[3rem] relative">
+          <input
+            type="text"
+            placeholder="Search Markets"
+            className="px-4 py-2 text-black rounded-full w-full pr-16 text-sm bg-zinc-700 "
+          />
+          <button className="py-1.5 px-3.5 w-[6rem] h-[2.5rem] bg-purple-600 rounded-full hover:bg-purple-700 absolute right-1.5 top-1 text-sm">
+            Search
+          </button>
+        </div>
+              </main>
+    </div>
   );
-}
+};
+
+export default Home;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,42 +1,77 @@
-// components/Header.tsx
-import React from 'react';
+"use client"
 
+import React, { useEffect, useState } from 'react';
+import { fetchPrice } from '../../utils/api';
+
+/**
+ * Header component that displays cryptocurrency prices and a navigation menu.
+ */
 const Header: React.FC = () => {
+  const [bitcoinPrice, setBitcoinPrice] = useState<number | null>(null);
+  const [ethereumPrice, setEthereumPrice] = useState<number | null>(null);
+  const [lunaPrice, setLUNAPrice] = useState<number | null>(null);
+  const [tetherPrice, setTetherPrice] = useState<number | null>(null);
+  const [uniswapPrice, setUniswapPrice] = useState<number | null>(null);
+  const [dogecoinPrice, setDogecoinPrice] = useState<number | null>(null);
+
+
+    /**
+   * Fetches cryptocurrency prices from the API and updates the state.
+   */
+  useEffect(() => {
+    const getPrices = async () => {
+      const btcPrice = await fetchPrice("BTC");
+      const ethPrice = await fetchPrice("ETH");
+      const lunaPrice = await fetchPrice("LUNA");
+      const usdtPrice = await fetchPrice("USDT");
+      const uniPrice = await fetchPrice("UNI");
+      const dogePrice = await fetchPrice("DOGE");
+
+      setBitcoinPrice(btcPrice);
+      setEthereumPrice(ethPrice);
+      setLUNAPrice(lunaPrice);
+      setTetherPrice(usdtPrice);
+      setUniswapPrice(uniPrice);
+      setDogecoinPrice(dogePrice);
+    };
+
+    getPrices();
+  }, []);
   return (
     <>
-      <div className= " containter bg-neutral-800 text-white overflow-hidden p-1.5 text-sm">
+      <div className="bg-neutral-800 text-white overflow-hidden p-1.5 text-sm">
         <div className="marquee space-x-8 px-4 gap-10">
-          <div>Ethereum: 757.36 USD</div>
-          <div>Bitcoin: 993.32 USD</div>
-          <div>LUNA: 31.23 USD</div>
-          <div>Tether: 216.92 USD</div>
-          <div>Uniswap: 579.25 USD</div>
-          <div>Dogecoin: 831.36 USD</div>
+        <div>Ethereum: {ethereumPrice !== null ? `${ethereumPrice} USD` : "Loading..."}</div>
+          <div>Bitcoin: {bitcoinPrice !== null ? `${bitcoinPrice} USD` : "Loading..."}</div>
+          <div>LUNA: {lunaPrice !== null ? `${lunaPrice} USD` : "Loading..."}</div>
+          <div>Tether: {tetherPrice !== null ? `${tetherPrice} USD` : "Loading..."}</div>
+          <div>Uniswap: {uniswapPrice !== null ? `${uniswapPrice} USD` : "Loading..."}</div>
+          <div>Dogecoin: {dogecoinPrice !== null ? `${dogecoinPrice} USD` : "Loading..."}</div>
         </div>
-      </div>
+        </div>
       <header className="flex justify-between items-center p-6 bg-black text-white">
-        <div className="flex items-center space-x-4 gap-7 ml-5">
-          <img src="../favicon.ico" alt="Logo" className='w-8 h-8'/>
+        <div className="flex items-center space-x-4 ml-5">
+          <img src="../favicon.ico" alt="Logo" className="w-8 h-8" />
           <div className="font-bold">Morry</div>
           <nav className="space-x-4">
-            <a href="#" className="hover:underline text-sm">Home</a>
-            <a href="#" className="hover:underline text-sm">Service</a>
-            <a href="#" className="hover:underline text-sm">Market</a>
-            <a href="#" className="hover:underline text-sm">Community</a>
-            <a href="#" className="hover:underline text-sm">Blog</a>
-            <a href="#" className="hover:underline text-sm">About</a>
+            <a href="#" className="hover:underline text-sm ml-7">Home</a>
+            <a href="#" className="hover:underline text-sm ml-7">Service</a>
+            <a href="#" className="hover:underline text-sm ml-7">Market</a>
+            <a href="#" className="hover:underline text-sm ml-7">Community</a>
+            <a href="#" className="hover:underline text-sm ml-7">Blog</a>
+            <a href="#" className="hover:underline text-sm ml-7">About</a>
           </nav>
         </div>
         <div className="flex space-x-4 items-center">
           <div className="flex items-center space-x-1">
-            <select className="bg-black text-white border border-gray-600 rounded px-2 py-1">
-              <option value="en">ENG</option>
-              <option value="es">ESP</option>
-              <option value="fr">FRA</option>
-              <option value="de">DEU</option>
+            <select className="bg-transparent text-white border-gray-600 rounded px-2 py-1">
+              <option value="en" className="bg-zinc-700">ENG</option>
+              <option value="es" className="bg-zinc-700">ESP</option>
+              <option value="fr" className="bg-zinc-700">FRA</option>
+              <option value="de" className="bg-zinc-700">DEU</option>
             </select>
           </div>
-          <button className="px-4 py-2 bg-purple-600 rounded hover:bg-purple-700">Get Started</button>
+          <button className="px-4 py-2 w-[8rem] h-[2.5rem] bg-purple-600 rounded-full hover:bg-purple-700 text-sm">Get Started</button>
         </div>
       </header>
     </>
